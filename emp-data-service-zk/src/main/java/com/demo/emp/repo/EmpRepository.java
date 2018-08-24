1fd338994abec59e913db474cec72ca193a5a3b5
Balamurugankp <kpbala2001@gmail.com>
2018-08-24 17:05:14 +0530

    Revert "SpringCloud"
    This reverts commit a44634300cb97b324ecd66206c230ccc4d6911df.

--- emp-data-service-zk/src/main/java/com/demo/emp/repo/EmpRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.emp.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.emp.entity.Emp;
-
-public interface EmpRepository extends JpaRepository<Emp, Integer>{
-
-}
--- hr-service-feign/src/main/java/com/demo/emp/controller/IEmpRestController.java
+++
@@ -1,6 +0,0 @@
-package com.demo.emp.controller;
-
-public interface IEmpRestController {
-
-
-}
